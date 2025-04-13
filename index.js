760b9004e079cd0aec1eb29c97c8a69d8d1f3b4b
Prajakta Satpute <prajusatpute15@gmail.com>
2025-04-13 15:12:59 +0530

    changes in index.js and env

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ const router = require("./router/auth-router");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ecommerce-frontend-gamma-amber.vercel.app"
+]
+
 
 const corsOptions = {
-    origin: "http://localhost:5173",
-    method: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     credentials: true,
-}
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Backend is running!");
-  });
-  
+});
+
 app.use("/api/auth", router);
 app.use(errorMiddleware);
 
